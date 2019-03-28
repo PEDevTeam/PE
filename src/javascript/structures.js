6fa851725886bb779bbb899b5a593e838a7beb81
Andrew-Svedby <42776677+Andrew-Svedby@users.noreply.github.com>
2019-03-28 17:05:44 +0100

    Initial commit of Friend related code changes

--- src/javascript/structures.js
+++ src/javascript/structures.js
@@ -647,6 +647,22 @@ window.structures={
 				delete tasksBodyNewTeacher[Object.keys(tasksBodyNewTeacher)[i]];
 			}
 		}
+
+		// Friend tasks
+		var tasksList=window.tasksFriend;
+		if (vars.tasksFriend == null) {
+		    vars.tasksFriend = {};
+		}
+		for (var attribute in tasksList) {
+		    if (!(attribute in vars.tasksFriend)) {
+			vars.tasksFriend[attribute] = tasksList[attribute];
+		    }
+		}
+		for (var attribute in vars.tasksFriend) {
+		    if (!(attribute in tasksList)) {
+			delete vars.tasksFriend[attribute];
+		    }
+		}
 	},
 	setupCheer: function (){
 		var vars=State.active.variables;
@@ -703,7 +719,7 @@ window.structures={
 
 window.playerList={
 	name: "",
-	debugA: false,
+	debugA: true,
 	debugM: false,
 	debugQ: false,
 	debugN: 0,
@@ -758,6 +774,7 @@ window.playerList={
 	bribeIncrease: 10,
 	friendLastVisit: 0,
 	clothes: [],
+	clothesTmp: null,
 	gameSkill: 0,
 	fitness: 0,
 	femaleName: false, //new flag
@@ -893,7 +910,14 @@ window.friendList={
 	his_her: 'his',
 	His_Her: 'His',
 	his_hers: 'his',
-	His_Hers: 'His'
+	His_Hers: 'His',
+	gender: 'M',
+
+	snoop: 0,
+	daysSinceLastVisit: 0,
+	catchUp: 0,
+	admitWhatsWrong: 0,
+	admitLikingTrap: 0,
 },
 
 window.futaList={
