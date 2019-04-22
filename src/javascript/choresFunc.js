b8cf83ea5c35117650b19edd100968abd8fec976
MisterX421138 <43083285+MisterX421138@users.noreply.github.com>
2019-04-22 18:49:56 -0500

    Difficulty fixes and other bug fixes
    Being punished on hard mode now works as expected.
    Sleep in nightie forced on hard mode.
    Image fixed in fast food job
    Cheer friend events altered to fit new cheer schedule
    Can not flash chastity in cheer shorts
    Can not take chastity selfie unless wearing a skirt

--- src/javascript/choresFunc.js
+++ src/javascript/choresFunc.js
@@ -10,7 +10,8 @@ window.choresFunc={
 			var choreV = chores[Object.keys(choresList)[i]];
 			if (choreV.active && (choreC.daringRequired <= player.daring) && (choreC.perversionRequired <= player.perversion.guardian) && (choreC.perversionMax >= player.perversion.guardian)) {
 				ca++;
-				if ((choreV.dayPerformed + choreC.daysWait) >= time.day) {
+				console.log(choreV);
+				if ((choreV.dayPerformed + choreC.daysWait) >= time.day && (!choreV.hardFail)) {
 					cd++;
 				}
 			}
